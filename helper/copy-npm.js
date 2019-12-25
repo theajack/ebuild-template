b245840af8d4ffe492ca7e1fec9e5cf817e69d74
tackchen <tackchen@tencent.com>
2019-12-25 18:38:06 +0800

    feat: init

---
+++ helper/copy-npm.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+let config = require('../ebuild.config');
+
+let tranToEs5InNpm = config.tranToEs5InNpm;
+
+function main () {
+    copy();
+    if (tranToEs5InNpm) {
+        transEs6ByBabel();
+    }
+}
+
+function copy () {
+    gulp.src(['src/*'])
+        .pipe(gulp.dest('npm'));
+}
+
+function transEs6ByBabel () {
+    gulp.src('src/*.js')
+        .pipe(babel({presets: ['@babel/env']}))
+        .pipe(gulp.dest('npm'));
+}
+
+
+main();
